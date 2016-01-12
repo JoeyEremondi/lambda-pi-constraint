9117778c6d8493d8d73655471c08fc743da4b83b
Joey Eremondi <joey@eremondi.com>
2016-01-11 20:25:10 -0800

    Remove old Constraint types

--- src/Constraint.hs
+++ src/Constraint.hs
@@ -25,6 +25,8 @@ import qualified Unbound.LocallyNameless as LN
 
 import PatternUnify.Tm as Tm
 
+import qualified PatternUnify.Context as UC
+
 cToUnifForm :: Int -> Common.CTerm_ -> Tm.VAL
 cToUnifForm ii (Common.L _ tm) =
   case tm of
@@ -93,66 +95,24 @@ appToSpine (Common.App it ct) =
 --Other expression:
 appToSpine e = (e, [])
 
-type ConstrContext = [(Common.Name, ConType)]
+type ConstrContext = [(Common.Name, Tm.VAL)]
 type WholeEnv = (Common.NameEnv Common.Value_, ConstrContext)
 
---We index type variables as unique integers
-newtype TypeVar = TypeVar (UF.Point TypeRepr, Int)
 
-getUF (TypeVar x) = fst x
-
-instance Show TypeVar where
-  show (TypeVar (_, i)) = "<" ++ show i ++ ">"
-
-data TypeRepr =
-   BlankSlate
-   | TypeRepr ConType
-   | TypeFnRepr (Common.Type_ -> ConType)
-   deriving (Show)
-
-
---Type we can unify over: either a type literal
---Or a type variable
-data ConType =
-  VarType TypeVar --Type variable can represent a type
-  | LitType Common.Type_ --We look at literal types in our constraints
-  | PiType ConType ConTyFn --Sometimes we look at function types, where the types
-  | AppType ConTyFn ConType --Used to encode
-                       --may be variables we resolve later with unification
-  | NatType --Our built-in data constructors
-  | VecType ConType ConType
-  | EqType ConType ConType ConType
---  | Param (UF.Point TypeRepr) --Used as a placeholder to deal with functions
-                  --The point is just used as a unique identifier
-  deriving (Show)
 
 instance Show (UF.Point a) where
   show _ = "var"
 
---Wrapper for functions on types, whose values we may not know
---But instead determine from inference
-data ConTyFn =
-  TyFnVar TypeVar
-  | TyFn (Common.Type_ -> ConType)
-  deriving (Show)
 
-instance Show (Common.Type_ -> ConType) where
-  show f = "(\\(-1) -> " ++ (show $ f $ Common.vfree_ (Common.Quote ( -1)) )++ ")"
 
 --Initially, the only constraint we express is that two types unify
 data Constraint =
-  ConstrUnify ConType ConType WholeEnv
-  | TyFnUnify ConTyFn ConTyFn WholeEnv
-  | ConstrEvaluatesTo ConType Common.CTerm_ WholeEnv
+  Constraint Common.Region (UC.Entry)
 
 maybeHead :: [a] -> Maybe a
 maybeHead [] = Nothing
 maybeHead (h:_) = Just h
 
-instance Show Constraint where
-  show (ConstrUnify t1 t2 env) = "\n" ++ (show t1) ++ " === " ++ (show t2) ++ " |ENV: " ++ show (maybeHead $ snd env)
-  show (TyFnUnify t1 t2 env) = "\n" ++(show t1) ++ " === " ++ (show t2) ++ " |ENV: " ++ show ( maybeHead $ snd env)
-  show (ConstrEvaluatesTo t term env) = "\n" ++(show t) ++ " ~~> " ++ (show term) ++ " |ENV: " ++ show ( maybeHead $ snd env)
 
 
 
@@ -172,17 +132,16 @@ class Unifyable a where
   }-}
 
 
-type UFM = UF.UnionFindT TypeRepr Identity
 
-type ConstraintM a = WriterT [Constraint] (StateT [Int] UFM) a
+type ConstraintM a = WriterT [Constraint] (StateT [Int] Identity) a
 
 --Operations in our monad:
 --Make fresh types, and unify types together
-freshVar :: ConstraintM TypeVar
+freshVar :: ConstraintM Tm.VAL
 freshVar = do
   newInt <- freshInt
-  newPoint <- lift $ lift $ UF.fresh BlankSlate
-  return $  TypeVar (newPoint, newInt)
+  --newPoint <- lift $ lift $ UF.fresh BlankSlate
+  return $  error "TODO fresh Metavariable from Int"
 
 
 freshInt :: ConstraintM Int
@@ -191,54 +150,6 @@ freshInt = do
   put t
   return h
 
-instance Unifyable ConType where
-  unify t1 t2 env = addConstr (ConstrUnify t1 t2 env)
-  fresh = VarType <$> freshVar
-
-instance Unifyable ConTyFn where
-  unify t1 t2 env = addConstr (TyFnUnify t1 t2 env)
-  fresh = TyFnVar <$> freshVar
-
-evaluate :: Common.CTerm_ -> WholeEnv -> ConstraintM ConType
-evaluate term env = do
-  t <- fresh
-  addConstr $ ConstrEvaluatesTo t term env
-  return t
-
-
-unknownIdent :: String -> ConstraintM a
-unknownIdent s = error $ "Unknown Identifier: " ++ show s
-
-mkPi :: ConType -> ConTyFn -> ConType
-mkPi = PiType
-
-applyPi :: ConTyFn -> ConType -> ConType
-applyPi = AppType
-
-applyVal :: ConType -> ConType -> ConType
-applyVal f x = (valToFn f) `applyPi` x
-
-mkNat :: ConType
-mkNat = NatType
-
-mkVec :: ConType -> ConType -> ConType
-mkVec = VecType
-
-mkEq :: ConType -> ConType -> ConType -> ConType
-mkEq = EqType
-
-
-conType :: Common.Type_ -> ConType
-conType = LitType
-
-conTyFn :: (Common.Type_ -> ConType) -> ConTyFn
-conTyFn = TyFn
-
-liftConTyFn :: (Common.Type_ -> Common.Type_) -> ConTyFn
-liftConTyFn f = TyFn ( conType . f )
-
-valToFn :: ConType -> ConTyFn
-valToFn fcon = error "ValToFn" $ TyFn (\v -> AppType ( conTyFn $ \f -> conType $ f `Common.vapp_` v) fcon )
 
 
 --Helpful utility function
