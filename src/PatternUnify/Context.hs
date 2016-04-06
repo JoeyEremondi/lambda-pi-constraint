be85095ec4f4a7fd9c666c04358197bfe38ba53c
Joey Eremondi <joey@eremondi.com>
2016-04-06 15:20:19 -0700

    Add function to get info from entry

--- src/PatternUnify/Context.hs
+++ src/PatternUnify/Context.hs
@@ -165,6 +165,18 @@ data Entry  =  E Nom Type Dec EqnInfo
             |  Prob ProbId Problem ProblemState
   deriving (Show, Generic)
 
+entryInfo :: Entry -> EqnInfo
+entryInfo (E _ _ _ info) = info
+entryInfo (Prob _ prob _) = probInfo prob
+  where
+    probInfo :: Problem -> EqnInfo
+    probInfo (Unify (EQN _ _ _ _ info)) = info
+    probInfo (All params bnd) =
+      let
+        (_, innerProb) = unsafeUnbind bnd
+      in
+        probInfo innerProb
+
 instance Alpha Entry
 instance Subst VAL Entry
 
